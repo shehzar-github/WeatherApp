cbc587423fc93423dc4aaec1b57c207f126f8e5d
Shehzar Shakoor <sshakoor@in.ibm.com>
2019-05-11 12:04:33 +0530

    integrate weather API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  temp:any='';
+  weatherData:any;
+  hasData:boolean=false; 
+  hasError:boolean=false; 
+  API_KEY='51692a5857fac0f32a289d1532ef1bfd';
+  BASE_URL='https://api.openweathermap.org/data/2.5/';
+weather={};
+error:any;
+  url:any= this.BASE_URL+'/weather?q=bengaluru&units=metric&appid='+this.API_KEY;
+  
+  constructor(private httpClient: HttpClient){}
 
+  getWeatherData(city){
+    console.log('city=',city);
+    this.httpClient.get(this.BASE_URL+'/weather?q='+city+'&units=metric&appid='+this.API_KEY).subscribe((response)=>{
+      console.log("response:",JSON.stringify(response));
+      this.weather=response;
+      if(this.weather["cod"]=='200'){
+        this.hasData=true; 
+      }
+      else{
+        this.hasData=false;
+        //this.hasError=true;
+      }
+      
+    },
+    (error) => {
+      console.log(JSON.stringify(error))
+     this.error = error;
+     this.hasError=true;
+     this.hasData=false;
+
+    }
+  );
+  }
 }
